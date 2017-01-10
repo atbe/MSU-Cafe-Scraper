a680f4ba60e79ff7f169916d380f92a47739ccf6
@be <abeahmed2@gmail.com>
2017-01-09 20:03:34 -0500

    Backup all menus collected while keeping a main json file.

--- collect_menus_to_json.py
+++ collect_menus_to_json.py
@@ -1,17 +1,39 @@
+from pathlib import Path
+from time import time
+
 from CafeScraper.Scraper import Scraper
 import json
 
-scraper = Scraper()
-json_dump = json.dumps(list(scraper.cafes_dict.values()), default=lambda c: c.__dict__, sort_keys=True)
-print(json_dump)
+def collect_backup_and_dump():
 
-# save to file
-with open('menus.json', 'w') as fp:
-    print(json_dump, file=fp)
+    '''
+    Get the new json
+    '''
+    scraper = Scraper()
+    json_dump = json.dumps(list(scraper.cafes_dict.values()), default=lambda c: c.__dict__, sort_keys=True)
 
-#
-#
-# for cafe in scraper.cafes:
-#     #print(cafe)
-#     json_dump = json.dumps(cafe, default=lambda c: c.__dict__)
-#     print(json_dump)
+    '''
+    Backup this json
+    '''
+    # check if backup folder exists
+    BACKUP_PATH_NAME = "backup_menu_json"
+    cwd = Path('.')
+    if not (cwd / BACKUP_PATH_NAME).exists():
+        # create it if it doesn't
+        print("DEBUG: backup folder did not exist, creating now...")
+        (cwd / BACKUP_PATH_NAME).mkdir(mode=0o744)
+
+    # epoch time this new json was created in seconds rounded to the nearest integer
+    new_backup_file_name = str(int(time())) + '_menus_save.json'
+    absolute_path = str((cwd / BACKUP_PATH_NAME).absolute()) + '/' + new_backup_file_name
+    with open(absolute_path, 'w') as fp:
+        print(json_dump, file=fp)
+
+    '''
+    Distribute the new json
+    '''
+    with open('menus.json', 'w') as fp:
+        print(json_dump, file=fp)
+
+if __name__ == '__main__':
+    collect_backup_and_dump()
